9b7a43dd81047924feac620e0a9f175f70606d59
marey111 <marey111@126.com>
2019-07-02 20:06:07 +1000

    mid-term

---
+++ Midterm_Class/sample-midterm/q1/subsetList.c
@@ -0,0 +1,33 @@
+// subsetList.c 
+// Written by Ashesh Mahidadia, Jan 2018
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include "DLList.h"
+#include <stdbool.h>
+
+
+/* 
+    You will submit only this one file.
+
+    Implement the function "subsetList" below. Read the exam paper for 
+    detailed specification and description of your task.  
+
+    - DO NOT modify code in the file DLList.h . 
+    - You can add helper functions in this file.  
+    - DO NOT add "main" function in this file. 
+    
+*/
+
+
+int subsetList(DLList L1, DLList L2){
+
+	/* 
+	   Your solution here 
+
+	*/
+}
+
+
+
